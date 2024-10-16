b2ef34453a79e9e6433f4cf7ea5df5fe595d5c17
Prasoon Kumar <prasoonkumar467@gmail.com>
2024-10-17 02:44:03 +0530

    improve color on landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -20,7 +20,7 @@ const LandingPage = () => {
           {/* Get Started Button */}
           <button
             onClick={handleGetStarted}
-            className="px-8 py-3 w-2/4 border-4 border-blue-500 hover:text-white text-lg rounded-xl hover:bg-blue-500 transition duration-300"
+            className="px-8 py-3 w-2/4 border-4 border-[#7071bc] hover:text-white text-lg rounded-xl hover:bg-[#7071bc] transition duration-300"
           >
             Get Started
           </button>
@@ -28,10 +28,11 @@ const LandingPage = () => {
 
         {/* Spline 3D Design */}
         <iframe
-          src="https://my.spline.design/threadsfeatureheaderanimation-c9d007801a477348d2efcf7e55c24bec/"
+          src="https://my.spline.design/threadsfeatureheaderanimation-c9d007801a477348d2efcf7e55c24bec"
           frameborder="0"
           className="w-4/5 fixed right-0 h-full z-0"
         ></iframe>
+        
       </div>
       <div className="bg-black h-16 fixed bottom-0 right-0 z-20 w-40"></div>
     </div>

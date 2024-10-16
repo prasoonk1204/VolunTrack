e067723812866040b24ded56a6e6cb5018874bbe
Prasoon Kumar <prasoonkumar467@gmail.com>
2024-10-17 00:52:52 +0530

    changed title and added favicon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,14 @@ import { useNavigate } from "react-router-dom";
 const Header = ({ currentRole }) => {
   const navigate = useNavigate();
 
-
   return (
-    <header className="bg-gray-800 text-white p-4 w-full flex justify-between">
-      <h1 className="text-2xl px-6">VolunTrack</h1>
-      
+    <header className="bg-gray-800 text-white p-4 w-full flex justify-between items-center">
+      {/* Logo and Title */}
+      <div className="flex items-center ">
+        <img src="/logo.png" alt="Logo" className="h-10 w-10 mr-3" />{" "}
+        {/* Logo */}
+        <h1 className="text-2xl">VolunTrack</h1> {/* Heading */}
+      </div>
     </header>
   );
 };
